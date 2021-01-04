956cae8764a07c92384d7bfa90f7d35f9ca1fc0a
St_vn <41025049+St-vn@users.noreply.github.com>
2021-01-03 22:38:18 -0500

    Update API.lua

--- API.lua
+++ API.lua
@@ -1,8 +1,8 @@
 --[[
     API :
         
-        table object : Constructor(Instance : Part)
-            Creates a new hitbox to cast rays with.
+        table object : Constructor(Instance : Part, array : Offsets)
+            Creates a new hitbox to cast rays from, you could optionally use an array of vector offsets instead of attachments being parented to the part.
  
             void : hitbox:Cast(function : Callback, table : Filter)
                 Starts casting rays and would filter the specified filter list + the part object, would call the callback upon hitting a target.
