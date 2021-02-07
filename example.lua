8b05a2428ff1005b7840706465bd354ba7fb5c49
St_vn <41025049+St-vn@users.noreply.github.com>
2021-02-07 18:54:47 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local createHitbox = require(game.ReplicatedStorage.HitboxCaster)
 local hitbox = createHitbox(workspace.Part) -- creating new hitboxes would create new objects instead of giving reference to the existing one because it is better practice to use the constructor once
 
-hitbox:Cast(function(part, model, humanoid) -- parameters
+hitbox:Cast(function(part, model) -- parameters
     
 end, {workspace.Baseplate}) -- Start casting, ignores the baseplate and would call the callback upon hitting stuff instead of using signals
 
