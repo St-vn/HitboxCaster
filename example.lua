cdef4d7034b8b62e4dcc9d7a2dad0b74259b92e1
St_vn <41025049+St-vn@users.noreply.github.com>
2021-02-07 18:51:31 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local createHitbox = require(game.ReplicatedStorage.HitboxCaster2)
+local createHitbox = require(game.ReplicatedStorage.HitboxCaster)
 local hitbox = createHitbox(workspace.Part) -- creating new hitboxes would create new objects instead of giving reference to the existing one because it is better practice to use the constructor once
 
 hitbox:Cast(function(part, model, humanoid) -- parameters
